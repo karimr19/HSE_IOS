3c8dec4196769b5148c9e7ecf72f2ab7348c08b7
karim <mirak4319@gmail.com>
2021-09-21 15:03:16 +0300

    2 point

--- kirakhmatullinPW2/kirakhmatullinPW2/ViewController.swift
+++ kirakhmatullinPW2/kirakhmatullinPW2/ViewController.swift
@@ -8,39 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let settingsView = UIView()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .gray
         setupSettingsButton()
+        setupSettingsView()
     }
     override var preferredStatusBarStyle: UIStatusBarStyle {
     return .lightContent
     }
-    
+    private func setupSettingsView(){
+        view.addSubview(settingsView)
+        settingsView.translatesAutoresizingMaskIntoConstraints = false
+        settingsView.backgroundColor = .red
+        settingsView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        settingsView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        settingsView.topAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.topAnchor,
+            constant: 55
+        ).isActive = true
+        settingsView.trailingAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+            constant: -10
+        ).isActive = true
+        settingsView.alpha = 0
+    }
     private func setupSettingsButton() {
         let settingsButton = UIButton(type: .system)
         view.addSubview(settingsButton)
         settingsButton.setImage(
-        UIImage(named: "settings")?.withRenderingMode(.alwaysOriginal),
-        for: .normal
+            UIImage(named: "settings")?.withRenderingMode(.alwaysOriginal),
+            for: .normal
         )
         settingsButton.translatesAutoresizingMaskIntoConstraints = false
         settingsButton.topAnchor.constraint(
-        equalTo: view.safeAreaLayoutGuide.topAnchor,
-        constant: 15
+            equalTo: view.safeAreaLayoutGuide.topAnchor,
+            constant: 15
         ).isActive = true
         settingsButton.trailingAnchor.constraint(
-        equalTo: view.safeAreaLayoutGuide.trailingAnchor,
-        constant: -15 ).isActive = true
-        settingsButton.heightAnchor.constraint(equalToConstant: 30).isActive
-        = true
-        settingsButton.widthAnchor.constraint(equalTo:
-        settingsButton.heightAnchor).isActive = true
+            equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+            constant: -15
+        ).isActive = true
+        settingsButton.heightAnchor.constraint(equalToConstant: 30
+        ).isActive = true
+        settingsButton.widthAnchor.constraint(equalTo: settingsButton.heightAnchor).isActive = true
+        settingsButton.addTarget(self, action: #selector(settingsButtonPressed),
+        for: .touchUpInside)
         
     }
+    @objc
+    private func settingsButtonPressed() {
+        UIView.animate(withDuration: 0.1, animations: {
+            self.settingsView.alpha = 1 - self.settingsView.alpha
+    })
+    }
+    
 
 
 }
