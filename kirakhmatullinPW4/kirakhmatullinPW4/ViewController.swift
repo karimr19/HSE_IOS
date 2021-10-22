65b41f75b73a04ba4d528e87954eceb566b3fc2f
karim <mirak4319@gmail.com>
2021-10-22 14:21:27 +0300

    HW4 5 points

--- kirakhmatullinPW4/kirakhmatullinPW4/ViewController.swift
+++ kirakhmatullinPW4/kirakhmatullinPW4/ViewController.swift
@@ -13,30 +13,41 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     @IBOutlet weak var emptyCollectionLabel: UILabel!
     @IBOutlet weak var notesCollectionView: UICollectionView!
     
+    var notes: [Note] = [] {
+        didSet{
+            emptyCollectionLabel.isHidden = notes.count != 0
+            notesCollectionView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(createNote(sender:)))
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return notes.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell =
         collectionView.dequeueReusableCell(withReuseIdentifier:
         "NoteCell", for: indexPath) as! NoteCell
-        cell.titleLabel.text = "Yeah"
-        cell.descriptionLabel.text = "That's great"
+        let note = notes[indexPath.row]
+        cell.titleLabel.text = note.title
+        cell.descriptionLabel.text = note.description
+        cell.contentView.widthAnchor.constraint(equalToConstant: collectionView.bounds.size.width - collectionView.contentInset.left - collectionView.contentInset.right).isActive = true
         return cell
     }
     
     @objc func createNote(sender: UIBarButtonItem) {
         guard let vc = storyboard?.instantiateViewController(withIdentifier:
-            "NoteViewController") else {
+            "NoteViewController") as? NoteViewController else {
                 return
         }
+        vc.outputVC = self
         navigationController?.pushViewController(vc, animated: true)
     }
 }
